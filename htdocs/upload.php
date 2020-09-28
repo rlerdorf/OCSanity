207b941901c4ab0659c06ceff4b628a51d61b0a3
Rasmus Lerdorf <rasmus@lerdorf.com>
2020-09-28 13:52:38 +0000

    Block script hacks

--- htdocs/upload.php
+++ htdocs/upload.php
@@ -3,7 +3,12 @@ $uploadDir = '../uploads';
 header('Content-type: application/json');
 if (!empty($_FILES)) {
     $tmpFile = $_FILES['file']['tmp_name'];
-    $filename = tempnam($uploadDir, basename($_POST['ruleset']));
-    move_uploaded_file($tmpFile,$filename);
-    echo json_encode(['file' => basename($filename)]);
+    $contents = file_get_contents($tmpFile);
+    if(preg_match("@&lt;\s*script\s*&gt;@i", $contents)) {
+        echo json_encode(['file' => '/']);
+    } else {
+        $filename = tempnam($uploadDir, basename($_POST['ruleset']));
+        move_uploaded_file($tmpFile,$filename);
+        echo json_encode(['file' => basename($filename)]);
+    }
 }
